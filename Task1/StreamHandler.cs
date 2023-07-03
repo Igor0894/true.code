f190e97cf30876da9510728e8c6d2af4299f726a
Igor.Kornaukhov <3075579@gmail.com>
2023-07-03 17:38:09 +0300

    Add Task1

---
+++ Task1/StreamHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace Task1
+{
+    public class StreamHandler
+    {
+        Stream Stream { get; set; }
+        int BufferSize { get; set; }
+        char EndOfMessage { get; set; }
+        public StreamHandler(Stream stream, int bufferSize, char endOfMessage)
+        {
+            this.Stream = stream;
+            this.BufferSize = bufferSize;
+            this.EndOfMessage = endOfMessage;
+        }
+        public byte[] ReadMessage()
+#warning Повторное использование приведёт к утере данных при окончании чтения сообщения не в tempBuffer.Length - 1
+        {
+            List<byte> buffer = new List<byte>();
+            byte[] tempBuffer = new byte[BufferSize];
+            bool messageEnded = false;
+            while (!messageEnded && Stream.Read(tempBuffer, 0, BufferSize) > 0)
+            {
+                foreach (byte b in tempBuffer)
+                {
+                    if (b != EndOfMessage) { buffer.Add(b); }
+                    else { messageEnded = true; break; }
+                }
+            }
+            return buffer.ToArray();
+        }
+    }
+}
