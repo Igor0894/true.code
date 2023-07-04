83a398436c6ded20d8fbd19cb815781f99731a12
Igor.Kornaukhov <3075579@gmail.com>
2023-07-04 09:24:42 +0300

    Success tests

--- Task2/MessageReader/Program.cs
+++ Task2/MessageReader/Program.cs
@@ -1,3 +1,5 @@
+using ApplicationServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IMessageFeeder>();
+builder.Services.AddSingleton<IMessageService, MessageService>();
 
 var app = builder.Build();
 
